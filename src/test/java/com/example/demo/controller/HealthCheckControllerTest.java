220693de3b7bc7c5ee1898434dd690be69916e29
Khalin94 <mpzx1102@gmail.com>
2023-07-07 20:03:48 +0900

    그저 커버리지를 위한 테스트 작성  - UserControllerTest 추가

---
+++ src/test/java/com/example/demo/controller/HealthCheckControllerTest.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureTestDatabase
+@AutoConfigureMockMvc
+@SpringBootTest
+class HealthCheckControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @DisplayName("health_check.html에 접근하면 200을 반환한다.")
+    @Test
+    void healthCheckTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/health_check.html"))
+                .andExpect(status().isOk());
+    }
+
+}
